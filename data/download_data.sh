37c1685e19fe60c7935bf9208cf87e73b28164a8
Konstantin Schürholt <55194201+kschuerholt@users.noreply.github.com>
2023-01-12 18:30:32 +0100

    Update data links

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 clear
-echo Downloading Hyper-Representations from https://zenodo.org/record/7261342/files/hyper_reps.zip?download=1
+echo Downloading Hyper-Representations from https://zenodo.org/record/7529960/files/hyper_reps.zip?download=1
 
-curl -# -o "hyper_reps.zip" "https://zenodo.org/record/7261342/files/hyper_reps.zip?download=1"
+
+curl -# -o "hyper_reps.zip" "https://zenodo.org/record/7529960/files/hyper_reps.zip?download=1"
 
 echo Unzipping hyper_reps
 
 unzip hyper_reps.zip
 
-echo Downloading pre-packaged zoos from https://zenodo.org/record/7261342/files/zoos.zip?download=1
+echo Downloading pre-packaged zoos from https://zenodo.org/record/7529960/files/zoos.zip?download=1
 
-curl -# -o "zoos.zip" "https://zenodo.org/record/7261342/files/zoos.zip?download=1"
+curl -# -o "zoos.zip" "https://zenodo.org/record/7529960/files/zoos.zip?download=1"
 
 echo Unzipping zoos
 
